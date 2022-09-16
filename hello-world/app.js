b27e6352fb7d1de682f9e2a499ecfec0a9c289a7
Dewsara-Insighture <dewsara.wijerathne@insighture.com>
2022-09-16 11:23:21 +0530

    initial commit

---
+++ hello-world/app.js
@@ -0,0 +1,23 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const lambdaHandler = async (event) => {
+    let response;
+    try {
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'hello world',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+
+    return response;
+};
